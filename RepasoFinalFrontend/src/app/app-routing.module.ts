16d90e15cc7d519921a54a7363a6c654177b7c4e
Valeria Gonzalez <valeriayaquelinag@gmail.com>
2023-07-11 19:07:49 -0300

    Encuentro 4 frontend 11/7/2023

--- RepasoFinalFrontend/src/app/app-routing.module.ts
+++ RepasoFinalFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Pagina404Component } from './pagina404/pagina404.component';
+import { HomeComponent} from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home',component:HomeComponent},
+  {path:'**',component:Pagina404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
