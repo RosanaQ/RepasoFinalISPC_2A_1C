5c000d4249d2d526df4826024b31f3995c112f66
Valeria Gonzalez <valeriayaquelinag@gmail.com>
2023-07-18 15:54:05 -0300

    Encuentro 11 Front 18/07/2023

--- RepasoFinalFrontend/src/app/services/suscription.service.ts
+++ RepasoFinalFrontend/src/app/services/suscription.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {map, Observable} from 'rxjs';
 import {Suscription} from '../model/suscription.model';
 
 @Injectable({
@@ -10,10 +10,17 @@ import {Suscription} from '../model/suscription.model';
 export class SuscriptionService {
 url:string='http://localhost:8000/api/v1/servicios';
 url2:string='http://localhost:3000/servicios';
+url3:string="api/v1/servicios/";
   constructor(private http:HttpClient) { }
 /*Obtiene listado de servicios*/
 getAllSuscriptions():Observable<Suscription[]>{
-  return this.http.get<Suscription[]>(this.url2);
+  return this.http.get<Suscription[]>(this.url3)
+  .pipe(map(suscripciones=>suscripciones.map(item=>{
+    return{
+      ...item,
+      DescripcionList:item.Descripcion_S.split(";")
+    }
+  })));
 } 
 
 }
